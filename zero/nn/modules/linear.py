replaced requires with install_requires in setup.py moved modules into nn module for farther updates improved code readability fixed backpropagation

zero/nn/modules/linear.py
@@ -8,17 +8,22 @@ class Linear(Layer):
     def __init__(self, input_dim: int, output_dim: int):
         self.weights = np.random.randn(output_dim, input_dim)
         self.bias = np.random.randn(output_dim, 1)
+        self.input_matrix = None
 
-    def forward(self, x: np.ndarray):
+    def forward(self, input_matrix: np.ndarray):
         """ Forward propagation method. """
-        self.x = x
-        output = self.weights @ x
+        self.input_matrix = input_matrix
+        output = self.weights @ input_matrix
         output += self.bias
         return output
     
-    def backward(self, output_gradient: np.ndarray, lr: float):
+    def backward(self, output_gradient: np.ndarray, learning_rate: float):
         """ Back propagation method. """
-        self.weights -= lr * output_gradient @ self.x.T
-        self.bias -= lr * output_gradient
+        # Update self parameters
+        weights_gradient = output_gradient @ self.input_matrix.T
+        self.weights -= learning_rate * weights_gradient
+        self.bias -= learning_rate * output_gradient
+
+        # Pass output gradient to previous layer
         output_gradient = self.weights.T @ output_gradient
         return output_gradient