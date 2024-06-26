replaced requires with install_requires in setup.py moved modules into nn module for farther updates improved code readability fixed backpropagation

zero/nn/modules/activation.py
@@ -3,9 +3,9 @@ from .layer import Layer
 
 
 class ReLU(Layer):
-    def forward(self, x: np.ndarray):
-        self.x = x
-        return max(0, x)
+    def forward(self, input_matrix: np.ndarray):
+        self.input_matrix = input_matrix
+        return np.maximum(0, input_matrix)
     
-    def backward(self, output_gradient: np.ndarray):
-        return output_gradient * (self.x >= 0)
+    def backward(self, output_gradient: np.ndarray, learning_rate: float):
+        return learning_rate * output_gradient * (self.input_matrix > 0)

zero/nn/modules/linear.py
@@ -6,9 +6,11 @@ class Linear(Layer):
     """ Linear layer of neural network. 
     Performs linear transformation: wX + b. """
     def __init__(self, input_dim: int, output_dim: int):
+        self.input_dim = input_dim
+        self.output_dim = output_dim
         self.weights = np.random.randn(output_dim, input_dim)
         self.bias = np.random.randn(output_dim, 1)
-        self.input_matrix = None
+        self.input_matrix: np.ndarray
 
     def forward(self, input_matrix: np.ndarray):
         """ Forward propagation method. """
@@ -16,13 +18,15 @@ class Linear(Layer):
         output = self.weights @ input_matrix
         output += self.bias
         return output
-    
+
     def backward(self, output_gradient: np.ndarray, learning_rate: float):
         """ Back propagation method. """
         # Update self parameters
         weights_gradient = output_gradient @ self.input_matrix.T
         self.weights -= learning_rate * weights_gradient
-        self.bias -= learning_rate * output_gradient
+
+        # NOTE: check bias update or replace with better
+        self.bias = output_gradient.mean(axis=1).reshape(self.output_dim, 1)
 
         # Pass output gradient to previous layer
         output_gradient = self.weights.T @ output_gradient