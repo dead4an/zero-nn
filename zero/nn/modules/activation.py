fixed updating bias in linear layer, now gradient's calculated correctly fixed mse: now backward returns dE/dY* (np.ndarray) updated ReLU: added type hint for self. input matrix in initialization; replaced > with >= in backpropagation

zero/nn/modules/activation.py
@@ -3,9 +3,12 @@ from .layer import Layer
 
 
 class ReLU(Layer):
+    def __init__(self):
+        self.input_matrix: np.ndarray
+        
     def forward(self, input_matrix: np.ndarray):
         self.input_matrix = input_matrix
         return np.maximum(0, input_matrix)
     
     def backward(self, output_gradient: np.ndarray, learning_rate: float):
-        return learning_rate * output_gradient * (self.input_matrix > 0)
+        return learning_rate * output_gradient * (self.input_matrix >= 0)

zero/nn/modules/linear.py
@@ -24,9 +24,7 @@ class Linear(Layer):
         # Update self parameters
         weights_gradient = output_gradient @ self.input_matrix.T
         self.weights -= learning_rate * weights_gradient
-
-        # NOTE: check bias update or replace with better
-        self.bias = output_gradient.mean(axis=1).reshape(self.output_dim, 1)
+        self.bias -= output_gradient.sum(axis=1, keepdims=True)
 
         # Pass output gradient to previous layer
         output_gradient = self.weights.T @ output_gradient

zero/nn/modules/loss.py
@@ -10,4 +10,4 @@ class MSE(Layer):
         return (1 / len(y_true)) * np.sum(np.power(y_pred - y_true, 2))
     
     def backward(self):
-        return (2 / len(self.y_pred)) * np.sum(self.y_pred - self.y_true)
+        return (len(self.y_true) / 2) * (self.y_pred - self.y_true)